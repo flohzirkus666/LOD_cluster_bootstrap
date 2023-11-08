4589c417c3ccd054006eb818d855ec4054c73de8
Pascal Scheiben <flohzirkus666@gmail.com>
2023-11-08 20:15:14 +0100

    Fixing bash script, adding lNLFs

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,13 +1,22 @@
-!# /bin/bash
+#!/bin/bash
 
-cd
+cd ~
+
+echo "Installing missing packages"
+yum install -y python3-pip
+
+echo "Upgrading pip"
+python3 -m pip install --upgrade pip
+
+echo "Installing Ansible"
+python3 -m pip install ansible
+
+echo "Installing Python dependencies"
+python3 -m pip install netapp_lib netapp.ontap
 
 echo "Cloning custom galaxy repo"
 git clone -b feature/enhanced_cluster_setup https://github.com/flohzirkus666/netapp.ontap
 
 echo "Installing galaxy collection"
 ansible-galaxy collection install netapp.ontap
-
-echo "Installing Python dependencies"
-pip3 install netapp_lib netapp.ontap
 
